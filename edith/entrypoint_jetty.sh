a00ed6f759740402e6dba3e0f4fb6c4481e54c44
Niklas Liljestrand <niklas.liljestrand@gmail.com>
2018-05-10 22:34:04 +0300

    first working candidate

--- edith/entrypoint_jetty.sh
+++ edith/entrypoint_jetty.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ ! -f "/opt/svn/edith/init" ]; then
+chown -R www-data:www-data /opt/svn
+if [ ! -f "/opt/svn/init.done" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   svn mkdir -m "creating dirs" file:///opt/svn/edith/documents/
   svn mkdir -m "creating dirs" file:///opt/svn/edith/documents/trunk/
-  touch /opt/svn/edith/init
+  touch /opt/svn/init.done
 fi
 java -jar /var/lib/jetty/start.jar -DFOREGROUND
